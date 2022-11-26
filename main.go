f51368dda183fb4a3affa7d0adfedd210b5b8ab3
Nanichang Katzing <nanipaul68@gmail.com>
2022-11-26 19:01:29 +0100

    crud completed

--- main.go
+++ main.go
@@ -41,6 +41,46 @@ func deleteMovie(res http.ResponseWriter, req *http.Request) {
 			break
 		}
 	}
+	json.NewEncoder(res).Encode(movies)
+}
+
+// Get a Movie by ID function
+func getMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(res).Encode(item)
+			return
+		}
+	}
+}
+
+// Create a Movie
+func createMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(req.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(res).Encode(movie)
+}
+
+// Update a Movie
+func updateMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(req.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(res).Encode(movie)
+			return
+		}
+	}
 }
 
 func main() {
@@ -48,11 +88,11 @@ func main() {
 
 	// Add a dummy movie
 	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Black Panther", Director: &Director{Firstname: "Nani", Lastname: "Paul"}})
-	movies = append(movies, Movie{ID: "1", Isbn: "438627", Title: "Black Adam", Director: &Director{Firstname: "James", Lastname: "Bond"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438627", Title: "Black Adam", Director: &Director{Firstname: "James", Lastname: "Bond"}})
 
 	router.HandleFunc("/movies", getMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	router.HandleFunc("movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
 	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
